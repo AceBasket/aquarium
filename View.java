8aaa66284c8480064793c3ca8cea9c74318312e5
Sarah Mainguet <sarah.mainguet@bordeaux-inp.fr>
2023-03-19 22:35:16 +0100

    beginning of communication with controller

--- View.java
+++ View.java
@@ -20,6 +20,50 @@ public class View {
         output = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
     }
 
+
+    private void init() {
+        if (this.id != 0) {
+            this.output.println("hello in as " + this.id);
+        } else {
+            this.output.println("hello");
+        }
+    }
+
+    private void getFishes() {
+        this.output.println("getFishes");
+    }
+
+    private void getFishesContinuously() {
+        this.output.println("getFishesContinuously");
+    }
+
+    private void getListFishes() {
+        this.output.println("ls");
+    }
+
+    private void logOut() {
+        this.output.println("log out");
+    }
+
+    private void ping() {
+        this.output.println("ping " + this.id);
+    }
+
+    // prototype not fixed
+    private void addFish(String name, String destination, String size, String path) {
+        this.output.println("addFish  " + name + " at " + destination + ", " + size + ", " + path);
+    }
+
+    // prototype not fixed
+    private void delFish(String name) {
+        this.output.println("delFish " + name);
+    }
+
+    // prototype not fixed
+    private void startFish(String name) {
+        this.output.println("delFish " + name);
+    }
+
     public static void main(String[] argv) {
         try {
 
