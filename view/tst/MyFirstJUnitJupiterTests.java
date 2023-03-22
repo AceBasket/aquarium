5569560a2c7557ca49e3298ba2efec547e4be187
Sarah Mainguet <sarah.mainguet@bordeaux-inp.fr>
2023-03-22 17:31:17 +0100

    new file organization

---
+++ view/tst/MyFirstJUnitJupiterTests.java
@@ -0,0 +1,13 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+/* Documentation on : https://junit.org/junit5/docs/current/user-guide/#writing-tests */
+
+class MyFirstJUnitJupiterTests {
+
+    @Test
+    void addition() {
+        assertEquals(2, 1 + 1);
+    }
+}
