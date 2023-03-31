c2916426eaa433d2b7401f7a5ff5d25df090f448
EL OSROUTI Fatima_Ezahrae <Fatima_Ezahrae.El_Osrouti@bordeaux-inp.fr>
2023-03-31 09:19:13 +0200

    organisation

---
+++ controller/src/utils/utils.c
@@ -0,0 +1,14 @@
+#include "utils.h"
+
+void exit_if(int condition, const char *prefix)
+{
+    if (condition) {
+        if (errno != 0) {
+            perror(prefix);
+        }
+        else {
+            fprintf( stderr, "%s\n", prefix );
+        }
+        exit(1);
+    }
+}
---
+++ controller/src/utils/utils.h
@@ -0,0 +1,5 @@
+#ifndef _UTILS_H_
+#define _UTILS_H_
+
+void exit_if(int condition, const char *prefix);
+#endif
