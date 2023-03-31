ed6d8f0e804960b0cc519a068e4af9e802a11e20
Sarah Mainguet <sarah.mainguet@bordeaux-inp.fr>
2023-03-31 10:52:22 +0200

    new test orga

---
+++ controller/tst/test.c
@@ -0,0 +1,70 @@
+#include "test.h"
+
+int main(int argc, char *argv[]) {
+    fct_test_parse();
+
+
+    // srand(time(NULL));
+    // char options[] = ":t:";
+    // int opt;
+    // int test_demand = 0;            //0 = All tests
+
+    // while ((opt = getopt(argc, argv, options)) != -1) {
+    //     switch (opt) {
+    //     case 't':
+    //         test_demand = atoi(optarg);
+    //         break;
+
+    //     default:
+    //         break;
+    //     }
+    // }
+
+    // switch (test_demand) {
+    // case 0:
+    //     fct_test_matrix(0);
+    //     fct_test_player_utilities();
+    //     fct_test_coloration();
+    //     fct_test_print_game();
+    //     fct_test_player_Luca();
+    //     fct_test_player_Tanguy();
+    //     fct_test_player_Sarah();
+    //     fct_test_player_Cassie();
+    //     break;
+
+    // case 1:
+    //     fct_test_matrix(1);
+    //     break;
+
+    // case 2:
+    //     fct_test_player_utilities();
+    //     break;
+
+    // case 3:
+    //     fct_test_print_game();
+    //     break;
+
+    // case 4:
+    //     fct_test_coloration();
+    //     break;
+
+    // case 5:
+    //     fct_test_player_Luca();
+    //     break;
+
+    // case 6:
+    //     fct_test_player_Tanguy();
+    //     break;
+
+    // case 7:
+    //     fct_test_player_Sarah();
+    //     break;
+
+    // case 8:
+    //     fct_test_player_Cassie();
+    //     break;
+
+    // default:
+    //     break;
+    // }
+}
