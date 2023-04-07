adedbc0d1f10e8d8ddb66d1eda52b7fe1f8a78d5
EL OSROUTI Fatima_Ezahrae <Fatima_Ezahrae.El_Osrouti@bordeaux-inp.fr>
2023-04-07 22:56:33 +0200

    finishing parser

--- controller/src/parser.c
+++ controller/src/parser.c
@@ -6,11 +6,12 @@
 #include <assert.h>
 #include <ctype.h>
 
+enum func {LOAD, SHOW, ADD_VIEW, DEL_VIEW, SAVE, STATUS, ADDFISH, DELFISH, STARTFISH, HELLO, GETFISHES, GFCONTINUOUSLY, LS, PING, LOG, UNKNOWN};
 
 struct parse{
     char **tab; // a list of parsed elements
     int size; // the number of elements(words, numbers...) in the tab
-    //enum function function_name;
+    enum func func_name;
 };
 
 void free_parser(struct parse *p){
@@ -58,8 +59,18 @@ void too_few_arguments(struct parse *p, char * str){
     printf("the commande should be like : %s\n", str);
     free_parser(p);
 }
+// int n_spaces(char *s){
+//     int c =0;
+//     for (int i =0; i< strlen(s); i++){
+//         if (s[c] != ' '){
+//         c+=1;
+//         }
+//     }
+//     return c;
+// }
 char *  remove_spaces(char *str)
 {
+    //int c = n_spaces(str);
     char *string =malloc(sizeof(char)*strlen(str));
     strcpy(string, str);
 	int i;
@@ -95,19 +106,14 @@ struct parse * parser(char *str) {
 
     // load 
     if (strcmp(command, "load") == 0) {
+        p->func_name = LOAD;
         char *arg = strtok(NULL, " ");
         if(arg==NULL || arg==0){
-            // printf("too fiew arguments! \n");
-            // printf("the command should be like: load <aquarium name>");
-            // free_parser(p);
             too_few_arguments(p, "load <aquarium name>");
             return NULL;
         }
 
-        p->tab = realloc(p->tab, sizeof(char*) * (p->size + 1));
-        p->tab[p->size] = malloc(strlen(command) + strlen(arg) + 1);
-        strcpy(p->tab[p->size], arg);
-        p->size++;
+        adding_arg_to_parse(p, arg);
         if (strtok(NULL, " ") != NULL){
             too_much_arguments(p, "load <aquaruim name>");
             return NULL;
@@ -116,11 +122,9 @@ struct parse * parser(char *str) {
     }
     //show aquarium
     else if (strcmp(command, "show") == 0) {
+        p->func_name =SHOW;
         char *arg = strtok(NULL, " ");
         if(arg == NULL){
-            // printf("too fiew arguments! \n");
-            // printf("the command should be like: show <aquarium name>\n");
-            // free_parser(p);
             too_few_arguments(p, "show <aquarium name>");
             return NULL;
         }
@@ -137,6 +141,7 @@ struct parse * parser(char *str) {
     }
     //add view
     else if (strcmp(command, "add") == 0) {
+        p->func_name =ADD_VIEW;
         char * command2 = strtok(NULL, " ");
         if(command2 == NULL){
             too_few_arguments(p, "add view N5 400x400+400+200");
@@ -230,6 +235,7 @@ struct parse * parser(char *str) {
     }
     // del view
     else if (strcmp(command, "del") == 0) {
+        p->func_name = DEL_VIEW;
         char * command2 = strtok(NULL, " ");
         if(command2 == NULL){
             // printf("too fiew arguments! \n");
@@ -269,6 +275,7 @@ struct parse * parser(char *str) {
     }
     //save
     else if (strcmp(command, "save") == 0) {
+        p->func_name = SAVE;
         char *arg = strtok(NULL, " ");
         if(arg == NULL){
             too_few_arguments(p, "save <aquarium name>");
@@ -285,6 +292,7 @@ struct parse * parser(char *str) {
     }
     //status
     else if (strcmp(command, "status") == 0) {
+        p->func_name = STATUS;
         if (strtok(NULL, " ") != NULL){
             too_much_arguments(p, "status");
         }
@@ -292,6 +300,7 @@ struct parse * parser(char *str) {
     }
     //addFish
     else if (strcmp(command, "addFish") == 0) {
+        p->func_name = ADDFISH;
         char *arg1 = strtok(NULL, " ");
         if(arg1 == NULL){
             too_few_arguments(p, "addFish <nameFish> at <number>x<number>, <number>x<number>, <path>>");
@@ -350,14 +359,19 @@ struct parse * parser(char *str) {
             too_few_arguments(p, "addFish <nameFish> at <number>x<number>, <number>x<number>, <path>");
             return NULL;
         }
-        arg4 = remove_spaces(arg4); // allocation 
-        if(!is_number(arg4, 0)){
+        //arg4 = remove_spaces(arg4); // allocation 
+        if(!is_number(arg4, 1)){
             printf("the 4th(%s) coord should be a number\n", arg4);
             printf("Or the separation between the 3nd argument and 4th should be an <x> symbol\n");
             free_parser(p);
+            //free(arg4);
             return NULL;
         }
+        
+        // p->size++;
+        //adding_arg_to_parse(p, remove_spaces(arg4));
         adding_arg_to_parse(p, arg4);
+        //free(arg4);
 
         char *arg5 = strtok(NULL, ", ");
         if(arg5 == NULL){
@@ -372,80 +386,179 @@ struct parse * parser(char *str) {
         }
         adding_arg_to_parse(p, arg5);
 
-        char *arg6 = strtok(NULL, "");
-        arg6 = remove_spaces(arg6);
+        char *arg6 = strtok(NULL, " ");
+        
         if(arg6 == NULL){
             too_few_arguments(p, "addFish <nameFish> at <number>x<number>, <number>x<number>, <path>");
             return NULL;
         }
+        //arg6 = remove_spaces(arg6);
         if(!is_alphanum(arg6)){
             printf("the path(%s) name should be composed of alphabets and/or numbers\n", arg6);
             free_parser(p);
+            //free(arg6);
             return NULL;
         }
         adding_arg_to_parse(p, arg6);
-
+        //free(arg6);
         if(strtok(NULL, "") != NULL){
             too_much_arguments(p, "addFish <nameFish> at <number>x<number>, <number>x<number>, <path>");
+            return NULL;
         }
         return p;
     }
-    // else if (strcmp(command, "delFish") == 0) {
-    //     char *arg = strtok(NULL, "");
-    //     printf("delFish %s\n", arg);
-    //     return;
-    // }
-    // else if (strcmp(command, "startFish") == 0) {
-    //     char *arg = strtok(NULL, "");
-    //     printf("startFish %s\n", arg);
-    //     return;
-    // }
-    // else if (strcmp(command, "hello") == 0) {
-    //     command = strtok(NULL, " ");
-    //     if (command == NULL) {
-    //         printf("greetings xx\n");
-    //     }
-    //     if (strcmp(command, "in") != 0) {
-    //         printf("Command should be hello in as <ID>\n");
-    //         exit(0);
-    //     }
-    //     command = strtok(NULL, " ");
-    //     if (strcmp(command, "as") != 0) {
-    //         printf("Command should be hello in as <ID>\n");
-    //         exit(0);
-    //     }
-    //     char *ID = strtok(NULL, " ");
-    //     printf("greeting %s\n", ID);
-    //     return;
-    // }
-    // else if (strcmp(command, "getFishes") == 0) {
-    //     printf("printing periodically a list of fishes\n");
-    //     return;
-    // }
-    // else if (strcmp(command, "ls") == 0 || strcmp(command, "getFishesContinuously") == 0) {
-    //     printf("printing continuously a list of fishes\n");
-    //     return;
-    // }
-    // else if (strcmp(command, "ping") == 0) {
-    //     char *arg = strtok(NULL, " ");
-    //     printf("pong %d\n", atoi(arg));
-    //     return;
-    // } 
-    else
+    //delFish
+    else if (strcmp(command, "delFish") == 0) {
+        p->func_name = DELFISH;
+        char *arg = strtok(NULL, " ");
+        if(arg == NULL){
+            too_few_arguments(p, "delFish <nameFish>");
+            return NULL;
+        }
+        else if (!is_alphanum(arg)){
+            printf("the name of the fish should be composed of aplphabets and/or numbers");
+            free_parser(p);
+            return NULL;
+        }
+        if(strtok(NULL, "") !=NULL){
+            too_much_arguments(p, "delFish <nameFish>");
+            return NULL;
+        }
+        adding_arg_to_parse(p, arg);
+        return p;
+    }
+    //startFish
+    else if (strcmp(command, "startFish") == 0) {
+        p->func_name = STARTFISH;
+
+        char *arg = strtok(NULL, " ");
+        if(arg == NULL){
+            too_few_arguments(p, "startFish <nameFish>");
+            return NULL;
+        }
+        else if (!is_alphanum(arg)){
+            printf("the name of the fish should be composed of aplphabets and/or numbers");
+            free_parser(p);
+            return NULL;
+        }
+        if(strtok(NULL, "") !=NULL){
+            too_much_arguments(p, "startFish <nameFish>");
+            return NULL;
+        }
+        adding_arg_to_parse(p, arg);
+        return p;
+    }
+    //hello
+    else if (strcmp(command, "hello") == 0) {
+        p->func_name = HELLO;
+
+        command = strtok(NULL, " ");
+        if(command ==NULL){
+            return p;
+        }
+
+        if (strcmp(command, "in") != 0) {
+            printf("Command should be like : hello in as <ID> Or a simple hello\n");
+            free_parser(p);
+            return NULL;
+        }
+        adding_arg_to_parse(p, command);
+
+        command = strtok(NULL, " ");
+        if (command ==NULL || strcmp(command, "as") != 0) {
+            printf("Command should be like: hello in as <ID> or a simple hello\n");
+            free_parser(p);
+            return NULL;
+        }
+        adding_arg_to_parse(p, command);
+
+        char *ID = strtok(NULL, " ");
+        if(ID == NULL){
+            too_few_arguments(p, "hello in as <ID> or a simple hello");
+            return NULL;
+        }
+        else if (!is_number(ID, 0)){
+            printf("the ID(%s) should be a number!\n", ID);
+            return NULL;
+        }
+        else if (strtok(NULL, "") !=NULL){
+            too_much_arguments(p, "hello in as <ID> or a simple hello");
+            return NULL;
+        }
+        adding_arg_to_parse(p, ID);
+        return p;
+    }
+    //getFishes
+    else if (strcmp(command, "getFishes") == 0) {
+        p->func_name = GETFISHES;
+        if (strtok(NULL, " ") != NULL){
+            too_much_arguments(p, "getFishes");
+            return NULL;
+        }
+        return p;
+    }
+    //ls
+    else if (strcmp(command, "ls") == 0) {
+        p->func_name = LS;
+        if (strtok(NULL, " ") != NULL){
+            too_much_arguments(p, "ls");
+            return NULL;
+        }
+        return p;
+    }
+    //getFishesContinuously
+    else if (strcmp(command, "getFishesContinuously") == 0) {
+        p->func_name = GFCONTINUOUSLY;
+        if (strtok(NULL, " ") != NULL){
+            too_much_arguments(p, "getFishesContinuously");
+            return NULL;
+        }
+        return p;
+    }
+    //ping
+    else if (strcmp(command, "ping") == 0) {
+        p->func_name = PING;
+        if (strtok(NULL, " ") != NULL){
+            too_much_arguments(p, "ping");
+            return NULL;
+        }
+        return p;
+    } 
+    //log out
+    else if (strcmp(command, "log") == 0) {
+        p->func_name = LOG; 
+        command = strtok(NULL, " ");
+        if (command == NULL || strcmp(command, "out")){
+            printf("Command should be like: log out\n");
+            free_parser(p);
+            return NULL;
+        }
+        adding_arg_to_parse(p, command);
+        if (strtok(NULL, " ") != NULL){
+            too_much_arguments(p, "log out");
+            return NULL;
+        }
+        return p;
+    } 
+    else{
         printf("unknown command\n");
+        p->func_name = UNKNOWN;
+    }
     free_parser(p);
     return NULL;
 }
 
 int main(int argc, char * argv[]){
-    struct parse *result = parser("addFish PoissonNain at 61x52, 4x3, RandomWayPoint");
+    struct parse *result = parser("load AQUA");
     if (result !=NULL){
         printf("size %d\n", result->size);
+        printf("%d\n", result->func_name);
         for (int i =0; i<result->size; i++){
             printf("%s\n", result->tab[i]);
         }
     }
     else 
         printf("%p\n", result);
+    //printf("%d\n", atoi(result->tab[5]));
     return 0;
 }
