7056105299820755d020a6436a80bc9241019c12
Amande128 <amandine.solignac@bordeaux-inp.fr>
2023-04-12 16:00:50 +0200

    Avancée du Parser java

--- view/src/utils/Parse.java
+++ view/src/utils/Parse.java
@@ -21,12 +21,11 @@ public class Parse {
             
     }
     public static String[] parserCommand(String command) {
-        String wantedAdd = "addFish";
-        String commandAdd = command.substring(0, wantedAdd.length());
+        String commandAdd = command.split("addFish");
         String wantedDel = "delFish";
-        String commandDel = command.substring(0, wantedDel.length());
+        String commandDel = command.split("delFish");
         String wantedStart = "startFish";
-        String commandStart = command.substring(0, wantedStart.length());
+        String commandStart = command.split("startFish");
 
         String[] arrayRes;
         arrayRes = new String[10];
@@ -36,41 +35,34 @@ public class Parse {
         }
         else if (commandAdd.equals("addFish")) {
             arrayRes[0] = "1";
-            int i = wantedAdd.length();
-            String commandAt = command.substring(i, i+1);
-            while(!commandAt.equals("at")) {
-                i++;
-            }
-            arrayRes[1] = command.substring(wantedAdd.length(), i-1);
-            i = i+2;
-            int j = i;
-            String commandVir = command.substring(i);
-            while(!commandVir.equals(",") && i<coommand.length()) {
-                i++;
-            }
-            arrayRes[2] = command.substring(j, i-1);
-            arrayRes[3] = command.substring(i+1);
-            for (int k = 0; k < 4 ; k++) {
-                System.out.println(arrayRes[k]);
-            }
-            System.out.println("OK : addFish");
+            // Comment analyser les léléments entre les virgules avec split() ? Parce que là je vois pas
+
+            // System.out.println("OK : addFish");
         }
         else if (commandDel.equals("delFish")) {
-            System.out.println("OK : delFish");
+            // System.out.println("OK : delFish");
             arrayRes[0] = "2";
             arrayRes[1] = commandDel.substring(wantedDel.length()+1);
         }
         else if (commandStart.equals("startFish")) {
-            System.out.println("OK : startFish");
+            // System.out.println("OK : startFish");
             arrayRes[0] = "3";
             arrayRes[1] = commandStart.substring(wantedStart.length()+1);
         }
         else {
-            System.out.println("NOK : commande introuvable");
+            // System.out.println("NOK : commande introuvable");
             arrayRes[0] = "-1";
         }
         
         return arrayRes;
+    }
+
+    public static String parserServerResponse(String response) {
+        String responseNOK = response.split("NOK");
+        String responseOK = response.split("OK");
+        if (responseNOK.equals("NOK")) {
+            return response.substring(responseNOK.length() + 3); //On retourne l'erreur correspondante
+        }
     }
 
     
@@ -113,45 +105,46 @@ public class Parse {
     public static String parserResources(File file) throws IOException {
         return parserManager(file, "resources");
     }
-    public static void parserComm(String sent, String received) {
-        String hello = "hello";
-        String greeting = "greeting";
-        String sentHello = sent.substring(0, hello.length());
-        String ping = "ping";
-        String sentPing = sent.substring(0, ping.length());
-        if (sentHello.equals("hello")) {
-            String receivedGreeting = received.substring(0, greeting.length());
-            if (receivedGreeting.equals("greeting")) {
-                String ID = received.substring(greeting.length()+1);
-            }
-            else {
-                //Renvoyer une erreur
-            }
-        }
-        //Faire les list en réponse à getFishes, ls et getFishesContinuously
+
+    // public static void parserComm(String sent, String received) {
+    //     String hello = "hello";
+    //     String greeting = "greeting";
+    //     String sentHello = sent.substring(0, hello.length());
+    //     String ping = "ping";
+    //     String sentPing = sent.substring(0, ping.length());
+    //     if (sentHello.equals("hello")) {
+    //         String receivedGreeting = received.substring(0, greeting.length());
+    //         if (receivedGreeting.equals("greeting")) {
+    //             String ID = received.substring(greeting.length()+1);
+    //         }
+    //         else {
+    //             //Renvoyer une erreur
+    //         }
+    //     }
+    //     //Faire les list en réponse à getFishes, ls et getFishesContinuously
     
-        else if (sent.equals("log out")) {
-            if (received.equals("bye")) {
-                //Renvoyer ok
-            }
-            else {
-                //Renvoyer que la déconnexion n'a pas été faite
-            }
-        }
+    //     else if (sent.equals("log out")) {
+    //         if (received.equals("bye")) {
+    //             //Renvoyer ok
+    //         }
+    //         else {
+    //             //Renvoyer que la déconnexion n'a pas été faite
+    //         }
+    //     }
     
-        else if (sentPing.equals("ping")) {
-            String pong = "pong";
-            String receivedPong = received.substring(0, pong.length());
-            if (receivedPong.equals("pong")) {
-                //Retourner que la connexion est bonne
-            }
-            else {
-                //Renvoyer que problème de connexion
-            }
-        }
+    //     else if (sentPing.equals("ping")) {
+    //         String pong = "pong";
+    //         String receivedPong = received.substring(0, pong.length());
+    //         if (receivedPong.equals("pong")) {
+    //             //Retourner que la connexion est bonne
+    //         }
+    //         else {
+    //             //Renvoyer que problème de connexion
+    //         }
+    //     }
     
-        else {
-            //Retourner que la réponse n'est pas reconnue
-        }
-    }
+    //     else {
+    //         //Retourner que la réponse n'est pas reconnue
+    //     }
+    // }
 }
